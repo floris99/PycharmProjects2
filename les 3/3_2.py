c0a575b642191d2ceaaca63f05b095320a51d1b6
Floris <floris.spruijt@student.hu.nl>
2017-09-15 15:10:34 +0200

    opdracht

--- les 3/3_2.py
+++ les 3/3_2.py
@@ -1,2 +1,4 @@
 line1 = eval(input('geef je leeftijd:'))
-line2 =
+line2 = input('heb je een nederlands paspoort:')
+if line1 >=18 and 'ja' in line2:
+    print('gefeliciteerd u mag stemmen')
--- untitled1/dating age.py
+++
@@ -1,9 +0,0 @@
-
-
-def corn():
-    a = 2017
-    print(a)
-
-
-corn()
- lesson 16
